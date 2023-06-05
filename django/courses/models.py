b8718769f2e88ff23709bab8b7882249f50c794f
Ramji Subedi <ramjisubedi1991@gmail.com>
2023-06-05 11:51:04 +0545

    courses model created

--- django/courses/models.py
+++ django/courses/models.py
@@ -1,4 +1,11 @@
+import uuid
 from django.db import models
+
+class Sector(models.Model):
+    name = models.CharField(max_length=255)
+    sector_uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    related_course = models.ManyToManyField('Course')
+    sector_image = models.ImageField(upload_to='sector_image')
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
@@ -7,8 +14,24 @@ class Course(models.Model):
     updated = models.DateTimeField(auto_now=True)
     # author = models.ForeignKey(User)
     language = models.CharField(max_length=50)
-
-
+    course_section = models.ManyToManyField('CourseSection')
+    comments = models.ManyToManyField('Comment')
+    image_url = models.ImageField(upload_to='course_images')
+    course_uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    price =  models.DecimalField(max_digits=5, decimal_places=2)
 #Course
+
+class CourseSection(models.Model):
+    title = models.CharField(max_length=255)
+    episodes = models.ManyToManyField('Episode')
 #Course Section
+class Episode(models.Model):
+    title = models.CharField(max_length=255)
+    file=models.FileField(upload_to='course_video')
+    length=models.DateTimeField(max_length=10, decimal_places=2)
 #Episode
+
+class Comment(models.Model):
+    # user = models.ForeignKey(User)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
