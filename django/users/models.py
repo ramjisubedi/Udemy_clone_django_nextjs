75d220784b387bfc7701e0a8bb2a038bceb035d4
Ramji Subedi <ramjisubedi1991@gmail.com>
2023-06-05 17:00:40 +0545

    user app

--- django/users/models.py
+++ django/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_superuser(self, email, password,name,**other_fields):
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_superuser',True)
+
+        if other_fields.get('is_staff') is not True:
+            return ValueError('Superuser must have is_staff True')
+        
+        if other_fields.get('is_superuser') is not True:
+            return ValueError('Superuser must have is_superuser True')
+        
+        return self.create_user(email,password,name,other_fields)
+    
+    def create_user(self,email,password,name,**other_fields):
+        if not email:
+            raise ValueError('You must be provided valid email')
+        
+        email = self.email.normalize_email(email)
+
+        user = self.model(email=email,name=name, password=password,**other_fields)
+
+        user.set_password(password)
+
+        user.save()
+
+        return user
